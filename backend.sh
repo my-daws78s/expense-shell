a0d526a3e84618520db812de2e904e0f9efa6cae
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-23 07:05:08 +0530

    expense-shell practice.

--- backend.sh
+++ backend.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$TIMESTAMP-SCRIPT_NAME.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 Enter DB password:
 read -s mysql_secure_password
 
