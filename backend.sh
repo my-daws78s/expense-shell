96e07b04e1b86deca8261f2da4c3dc2f834653ae
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-23 06:24:29 +0530

    expense-shell practice.

---
+++ backend.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date-%F+%H+%M+%S)
+SCRIPT_NAME=$(echo $0 | cut -f "." -f1)
+LOGFILE=/tmp/$TIMESTAMP-SCRIPT_NAME.log
+Enter DB password:
+read -s mysql_secure_password
+
+#Colors:
+R="\e[31]"
+G="\e[32]"
+Y="\e[33]"
+B="\e[34]"
+N="\e[0]"
+
+echo -e "Starting Script at:: $B $TIMESTAMP $N"
+
+if [ USERID -ne 0 ]
+then
+    echo -e "$R User does not have root previleges. Hence cannot process from here. $N"
+    exit 1
+else
+    echo "User has root access."
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is $G SUCCESS. $N"
+    else
+        echo -e "$2 is $G FAILURE. $N"
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling nodejs:"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs:"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs:"
+
+#Need to handle idempotent nature
+id expense &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    echo "User expense is alreay created."
+    exit 1
+else 
+    useradd expense
+    VALIDATE $? "Created user expense: "
+
+echo -e "Finishing Script at:: $B $TIMESTAMP $N"
+
+#The below command using -p idempotency will be taken care
+mkdir -p /app &>>$LOGFILE
+VALIDATE $? "Created app directory:"
+
+cd /app
+rm -fr /app/*
+unzip /tmp/backend.zip &>>$LOGFILE
+VALIDATE $? "Extracted app directory:"
+
+npm install &>>$LOGFILE
+VALIDATE $? "Installing npm dependencies:"
+
+cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service &>>$LOGFILE 
+VALIDATE $? "Copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon reload:"
+
+systemctl start backend &>>$LOGFILE
+VALIDATE $? "Starting backend:"
+
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Enabling backend:"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing mysql:"
+
+#mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -p${mysql_secure_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h db.mydevops-learning.cloud -uroot -p${mysql_secure_password} < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Validating schema loading:"
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "Restarting backend:"
--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 USERID=$(id -u)
-mysql_secure_password="ExpenseApp@1"
+#mysql_secure_password="ExpenseApp@1"
+echo "Enter the mysql password:"
+read -s mysql_secure_password
+
 
 
 if [ $USERID -ne 0 ]
@@ -42,8 +45,18 @@ VALIDATE $? "Enabling mysqld"
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting of mysql"
 
-mysql_secure_installation --set-root-pass $mysql_secure_password &>>$LOGFILE
-VALIDATE $? "Setting up DB password"
+#Linux scripting is not idempotent by nature, user needs to take care:
+#Nature of program irrespective of how many times u run, it shud not change result.
+# mysql_secure_installation --set-root-pass $mysql_secure_password &>>$LOGFILE
+# VALIDATE $? "Setting up DB password"
+
+mysql -h localhost -uroot -p${mysql_secure_password} -e 'show databases;' &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ${mysql_secure_password} &>>$LOGFILE
+    VALIDATE $? "Setting up DB password: "
+else 
+    echo -e "mysql password is already setup. $Y SKIPPING $N"
 
 ###############################
 
