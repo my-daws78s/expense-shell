a6b4e5f8e1654d4af4faf70720aa63898dc321b7
Murali Krishna Vujji <vujjimuralikrishna@gmail.com>
2024-04-23 15:53:53 +0530

    expense-shell practice.

--- frontend.sh
+++ frontend.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 SCRIPTNAME=$(echo $? | cut -d "." -f1)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE=/tmp/$(SCRIPTNAME-TIMESTAMP).log
+LOGFILE=/tmp/$SCRIPTNAME-$TIMESTAMP.log
 
 R="\[31m"
 G="\[32m"
@@ -11,7 +11,7 @@ Y="\[33m"
 B="\[34m"
 G="\[0m"
 
-if[ $USERID -ne 0 ]
+if [ $USERID -ne 0 ]
 then
     echo "Pls login as root."
     exit 1
@@ -20,7 +20,7 @@ else
 fi
 
 VALIDATE(){
-    if[ $1 -ne 0 ]
+    if [ $1 -ne 0 ]
     then
         echo "$2 ..... is Failure "
     else 
@@ -28,8 +28,10 @@ VALIDATE(){
     fi
 }
 
+echo -e "Starting Script at:: $B $TIMESTAMP $N"
+
 dnf list installed nginx &>>$LOGFILE
-if[ $? -eq 0 ]
+if [ $? -eq 0 ]
 then
     echo -e "nginx is already installed... $Y SKIPPING $N"
 else 
@@ -46,8 +48,8 @@ VALIDATE $? "Starting nginx"
 curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloaded backend code"
 
-cd /usr/share/nginx/html
 rm -rf /usr/share/nginx/html/*
+cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracted backend code"
 
@@ -56,3 +58,5 @@ VALIDATE $? "Copied frontend service"
 
 systemctl restart nginx &>>$LOGFILE
 VALIDATE $? "Restarting nginx"
+
+echo -e "Finishing Script at:: $B $TIMESTAMP $N"
